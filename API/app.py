66850e67b6c0ae575762fae050e1530a315ca7a4
ma.parrat <ma.parrat@uniandes.edu.co>
2024-04-10 23:16:45 -0500

    app

--- API/app.py
+++ API/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, request
+from psycopg2 import Error
+import psycopg2
 
 app = Flask(__name__)
 
 
 
-@app.route('/REQ1', methods=['GET'])
+@app.route('/Task', methods=['GET'])
 def process():
    message = f"Hello,! (GET request)"
    return message
@@ -14,6 +16,26 @@ def process2():
    message = f"Hello,! (POST request)"
    return message
 
+def connection_db():
+    try:
+        connection = psycopg2.connect(
+            user="root",
+            password="2534946",
+            host="localhost",
+            port="5432",
+            database="fpvdb"
+        )
+        cursor = connection.cursor()
+        print("Conexión exitosa a PostgreSQL")
+        return connection, cursor
+    except (Exception, Error) as error:
+        print("Error al conectar a PostgreSQL:", error)
+
+def close_connection_db(connection, cursor):
+    if connection:
+        cursor.close()
+        connection.close()
 
 if __name__ == '__main__':
+  connection_db
   app.run(debug=True)
