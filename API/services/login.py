604301d9f0141fbfb38bf2e274cbc5901f457a4f
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2024-04-12 06:17:36 -0500

    feat(login): added login service

--- API/services/login.py
+++ API/services/login.py
@@ -1,11 +1,15 @@
 from flask import request
 from flask_jwt_extended import create_access_token
 from flask_restful import Resource
-
+from sqlalchemy import and_
+from models.model import db_session, User
 
 class LogIn(Resource):
 
-    
     def post(self):
-        token_de_acceso = create_access_token(identity=request.json["id"])
-        return {"usuario": request.json["usuario"], "token": token_de_acceso}
+        user = db_session.query(User).filter(User.email == request.json["email"],
+                                             User.password == request.json["password"]).first()
+        if user is None:
+            return "Check the data entered", 404
+        token_de_acceso = create_access_token(identity=user.id)
+        return {"token": token_de_acceso}
