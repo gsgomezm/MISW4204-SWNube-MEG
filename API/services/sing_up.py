1d27d0286986cdd1340e9fe90f1a0d728ce49b13
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2024-04-12 06:18:19 -0500

    feat(singup): added sing up service

---
+++ API/services/sing_up.py
@@ -0,0 +1,17 @@
+from flask import request
+from flask_jwt_extended import create_access_token
+from flask_restful import Resource
+from sqlalchemy import and_
+from models.model import db_session, User
+
+class SingUp(Resource):
+
+    def post(self):
+        if request.json["password1"] != request.json["password2"]:
+            return "The password are not equal",  406
+        new_user = User(username = request.json["username"],
+                        password = request.json["password1"],
+                        email = request.json["email"])
+        db_session.add(new_user)
+        db_session.commit()
+        return "Account created successfully", 200
