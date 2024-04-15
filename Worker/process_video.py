b9186baff362f843ba05f20289b92d7a9fe2a41f
GeraldineGomez <gs.gomezm1@uniandes.edu.co>
2024-04-15 00:17:48 -0500

    feat: feat: added celery logic to video processer worker

--- Worker/process_video.py
+++ Worker/process_video.py
@@ -1,5 +1,9 @@
 import os
 from update_video_db import update_video_status
+from celery import Celery
+
+
+celery_app = Celery('task', broker='redis://localhost:6379/0')
 
 
 actual_dir = os.path.dirname(os.path.abspath(__file__))[0: len(os.path.dirname(os.path.abspath(__file__))) - 7]
@@ -7,6 +11,7 @@ base_dir = os.path.join(actual_dir, "VideosFpv")
 editing_dir = os.path.join(actual_dir, "VideosFpv", "Editing")
 processed_dir = os.path.join(actual_dir, "VideosFpv", "Processed")
 
+@celery_app.task()
 def process_video(video_id, url_video):
     try:
         create_dirs()
@@ -15,13 +20,13 @@ def process_video(video_id, url_video):
         parts = video_filename.split(".mp4")
         name = parts[0]
         download_video(url_video, video_filename)
-        # file_concat_path = create_file_conc(name, video_filename)
-        # edit_video(video_filename, file_concat_path)
-        # remove_file("Editing", name + ".txt")
-        # remove_file("Editing", video_filename)
-        # remove_file("Editing", "c_" + video_filename)
-        # remove_file("Editing", "l_" + video_filename)
-        # update_video_status(video_id, "processed")
+        file_concat_path = create_file_conc(name, video_filename)
+        edit_video(video_filename, file_concat_path)
+        remove_file("Editing", name + ".txt")
+        remove_file("Editing", video_filename)
+        remove_file("Editing", "c_" + video_filename)
+        remove_file("Editing", "l_" + video_filename)
+        update_video_status(video_id, "processed")
     except OSError as e:
         print(f"Error: {e.strerror}")
 
@@ -30,8 +35,9 @@ def edit_video(video_filename, file_concat_path):
     file_c = os.path.join(editing_dir, "c_" + video_filename)
     file_l = os.path.join(editing_dir, "l_" + video_filename)
     file_p = os.path.join(processed_dir, video_filename)    
-    os.system('ffmpeg -f concat -safe 0 -i "' + file_concat_path + '" -c copy "' + file_c + '"')
-    os.system('ffmpeg -ss 0 -t 20  -i "' + file_path + '" -c copy "' + file_l + '"')
+    print(file_concat_path + " ddd" + file_c)
+    os.system('ffmpeg -f concat -safe 0 -i "' + file_concat_path + '" -c copy -movflags faststart "' + file_c + '"')
+    os.system('ffmpeg -ss 0 -t 20  -i "' + file_c + '" -c copy -movflags faststart "' + file_l + '"')
     os.system('ffmpeg -i "' + file_l + '" -c copy -aspect 16/9 "' + file_p + '"')
 
 def download_video(url_video, video_name):
