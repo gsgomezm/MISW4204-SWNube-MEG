916277bc0019aaba03a92980f5204c8a8f7fa029
gsgomezm <gs.gomezm1@uniandes.edu.co>
2024-04-21 16:28:00 -0500

    chore: added nfs config and redis

--- Worker/process_video.py
+++ Worker/process_video.py
@@ -2,14 +2,12 @@ import os
 from update_video_db import update_video_status
 from celery import Celery
 
-
 celery_app = Celery('task', broker='redis://localhost:6379/0')
 
-
 actual_dir = os.path.dirname(os.path.abspath(__file__))[0: len(os.path.dirname(os.path.abspath(__file__))) - 7]
-base_dir = os.path.join(actual_dir, "VideosFpv")
-editing_dir = os.path.join(actual_dir, "VideosFpv", "Editing")
-processed_dir = os.path.join(actual_dir, "VideosFpv", "Processed")
+base_dir = os.path.join(actual_dir, "Processing")
+editing_dir = os.path.join(actual_dir, "Processing", "Editing")
+processed_dir = os.path.join(actual_dir, "Processing", "Processed")
 
 @celery_app.task()
 def process_video(video_id, url_video):
@@ -34,20 +32,21 @@ def edit_video(video_filename, file_concat_path):
     file_path = os.path.join(editing_dir, video_filename)
     file_c = os.path.join(editing_dir, "c_" + video_filename)
     file_l = os.path.join(editing_dir, "l_" + video_filename)
-    file_p = os.path.join(processed_dir, video_filename)    
-    print(file_concat_path + " ddd" + file_c)
+    file_p = os.path.join(processed_dir, video_filename) 
+    destiny_path = os.path.join(actual_dir, "VideosFpv") 
     os.system('ffmpeg -f concat -safe 0 -i "' + file_concat_path + '" -c copy -movflags faststart "' + file_c + '"')
     os.system('ffmpeg -ss 0 -t 20  -i "' + file_c + '" -c copy -movflags faststart "' + file_l + '"')
     os.system('ffmpeg -i "' + file_l + '" -c copy -aspect 16/9 "' + file_p + '"')
+    os.system(f"sudo mv {file_path} {destiny_path}")
 
 def download_video(url_video, video_name):
     video_editing = os.path.join(editing_dir, video_name) 
-    print(video_editing)
     os.system(f"curl -o {video_editing} {url_video}")
     
 def create_file_conc(video_name, file_name):
     file_path = os.path.join(editing_dir, video_name + ".txt")
     content = "file logo.mp4\n" + "file " + file_name + "\n" + "file logo.mp4"
+    print(file_path)
     if not os.path.exists(file_path):
         with open(file_path, "w") as file:
             file.write(content)
@@ -57,10 +56,10 @@ def create_file_conc(video_name, file_name):
 def remove_file(folder_name, filename):
     file = os.path.join(base_dir, folder_name, filename)
     if  os.path.exists(file):
-        os.remove(file)
+        os.system(f"rm {file}")
 
 def create_dirs():
     if not os.path.exists(editing_dir):
-        os.mkdir(editing_dir)
+        os.system(f"mkdir {editing_dir}")
     if not os.path.exists(processed_dir):
-        os.mkdir(processed_dir)
+        os.system(f"mkdir {processed_dir}")
