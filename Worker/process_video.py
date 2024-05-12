cf562ce0418e89cfd8a282b904213199cd5df528
gsgomezm <gs.gomezm1@uniandes.edu.co>
2024-05-12 07:04:31 -0500

    feat: removed celery invocation

--- Worker/process_video.py
+++ Worker/process_video.py
@@ -1,9 +1,16 @@
 import os
 from bucket import upload_file
 from update_video_db import update_video_status
-from celery import Celery
+# from celery import Celery
 
-celery_app = Celery('task', broker='redis://localhost:6379/0')
+import argparse
+import typing
+from typing import Optional
+
+if typing.TYPE_CHECKING:
+    from google.pubsub_v1 import types as gapic_types
+
+# celery_app = Celery('task', broker='redis://localhost:6379/0')
 
 actual_dir = os.path.dirname(os.path.abspath(__file__))[0: len(os.path.dirname(os.path.abspath(__file__))) - 7]
 base_dir = os.path.join(actual_dir, "Processing")
@@ -13,7 +20,7 @@ processed_dir = os.path.join(actual_dir, "Processing", "Processed")
 downloaded_blob = "Downloaded"
 processed_blob = "Processed"
 
-@celery_app.task()
+# @celery_app.task()
 def process_video(video_id, url_video):
     try:
         create_dirs()
@@ -28,6 +35,7 @@ def process_video(video_id, url_video):
         remove_file("Editing", video_filename)
         remove_file("Editing", "c_" + video_filename)
         remove_file("Editing", "l_" + video_filename)
+        remove_file("Processed", video_filename)
         update_video_status(video_id, "processed")
     except OSError as e:
         print(f"Error: {e.strerror}")
@@ -39,10 +47,10 @@ def edit_video(video_filename, file_concat_path):
     file_p = os.path.join(processed_dir, video_filename) 
     # destiny_path = os.path.join(actual_dir, "VideosFpv")
     processed_blob_file = processed_blob + "/" + video_filename 
-    os.system('ffmpeg -f concat -safe 0 -i "' + file_concat_path + '" -c copy -movflags faststart "' + file_c + '"')
+    os.system('sudo ffmpeg -f concat -safe 0 -i "' + file_concat_path + '" -c copy -movflags faststart "' + file_c + '"')
     os.system('ffmpeg -ss 0 -t 20  -i "' + file_c + '" -c copy -movflags faststart "' + file_l + '"')
     os.system('ffmpeg -i "' + file_l + '" -c copy -aspect 16/9 "' + file_p + '"')
-    # os.system(f"sudo mv {file_path} {destiny_path}")
+    os.system(f"sudo mv {file_path} {destiny_path}")
     upload_file(processed_blob_file, file_path)
 
 def download_video(url_video, video_name):
